3a7f55087fa6fb790e199f54bee02132fd7396b6
HasegawaTakatune <0ta5274738t4p4r@gmail.com>
2023-01-14 20:04:58 +0900

    タブタイトル・メタディスクリプションの設定

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,41 +5,49 @@ const router = createRouter({
   routes: [
     {
       path: '/',
-      name: 'add-subract',
-      component: () => import('@/views/AddSubtaract.vue'),
-      meta: { title: 'add-subract', }
+      name: 'add-subtract',
+      component: () => import('@/views/AddSubtract.vue'),
+      meta: { title: 'Add/Subract', description: 'dayjs add or subtract sample' }
     },
     {
       path: '/startof-endof',
       name: 'startof-endof',
       component: () => import('@/views/StartOfEndOf.vue'),
-      meta: { title: 'startof-endof', }
+      meta: { title: 'Startof/Endof', description: 'dayjs startof or endof sample' }
     },
     {
       path: '/diff',
       name: 'diff',
       component: () => import('@/views/Diff.vue'),
-      meta: { title: 'diff', }
+      meta: { title: 'Diff', description: 'dayjs diff sample' }
     },
     {
       path: '/after-before-between',
       name: 'after-before-between',
       component: () => import('@/views/AfterBeforeBetween.vue'),
-      meta: { title: 'after-before-between', }
+      meta: { title: 'After/Before/Between', description: 'dayjs after or before or between sample' }
     },
     {
       path: '/locale',
       name: 'locale',
       component: () => import('@/views/Locale.vue'),
-      meta: { title: 'locale', }
+      meta: { title: 'Locale', description: 'dayjs locale sample' }
     },
     {
       path: '/time-zone',
       name: 'time-zone',
       component: () => import('@/views/TimeZone.vue'),
-      meta: { title: 'time-zone', }
+      meta: { title: 'Timezone', description: 'dayjs timezone sample' }
     }
   ]
+})
+
+const DEFAULT_TITLE = 'Dayjs Samples';
+router.afterEach((to, from) => {
+  // @ts-ignore
+  document.title = to.meta.title ?? DEFAULT_TITLE;
+  // @ts-ignore
+  document.querySelector("meta[name='description']").setAttribute('content', to.meta.description ?? 'Dayjs sample');
 })
 
 export default router
